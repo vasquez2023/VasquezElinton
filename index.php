db3497566d4c0e17092fec426366c2dc80d10b2d
vasquez2023 <andresvasquezv2015@gmail.com>
2023-04-29 19:16:05 -0500

    todo remplazado

--- index.php
+++ index.php
@@ -12,21 +12,21 @@ include("marco-right.php");
    <div  style="width: 100%;">     
     <div style="width: 100%; height:100%; margin: auto ; display:inline-block;" >
        <div style=" width:100%; height: 100%; top:0; font-size:20px; text-align:center; float:left;">
-        	<h1>¿Quién es Stalin Francis?</h1>
+        	<h1>¿Quién es Elinton Vasquez?</h1>
        		 <hr>
              
           <div id="posteo" style="text-align: left; font-size:0.5vw;">
                 Posteado por:  Vasquez Elinton<br>
-                Fecha: 2021-08-17 <br>
+                Fecha: 2023/4/29 <br>
           </div>
         </div>
     </div>
   </div>
   <div style="width: 100%; height:100%;" >
            <div  style="text-align:justify; font-size: 20px;">
-                   <p class="eys-parrafo">Hola, yo soy Stalin Adalberto <b> Francis Quinde </b>, ingeniero esmeraldeño que labora desde hace 10 años en la Universidad Tecnica Luis Vargas Torres, e impartido diferentes cátedras en la carrera de Tecnología de la Información entre las cuales esta Ingeniería de Software, Programación Web, Proyectos informáticos entre otras; pero la cátedra en la que mejor me desempeño es la de Fundamentos de programación que se dicta en el primer ciclo de la carrera de Tecnología de la Información. </p>
-                   <p class="eys-parrafo">Desde mís estudios secundarios me he apacionado por las ciencias y la tecnología, quieza sería  por esta capacidad que tiene estas dos áres del conocimiento de solucionar muchos probemas de la sociedad; es por eso que desde que termine mis estudios de ingeniería en la ESPOL, me he dedicado a la programación de computadoras. </p>
-<p class="eys-parrafo"> El primer lenguaje con el cual empece a programar en el tercer ciclo de universidad fue el <b><a href="https://en.wikipedia.org/wiki/BASIC"> basic</a></b>  para este tiempo la ESPOL contaba con laboratorios de computadora  una computadora personal de marca IBM de tip XT modelo 5160  en es.</p>
+                   <p class="eys-parrafo">Hola, yo soy Elinton Andres <b> Vasquez Vivero </b>, Estudiante esmeraldeño que crusa el 7 ciclo de la carreara de ingenieria en sistemas mi materia favorita es la gestion de base de datos y programacion aun que en esta me desempeño de manera regular . </p>
+                   <p class="eys-parrafo">En la secundaria me fui interesando mucho por el area informatica de hecho mi titulo de bachiller es en esta area, es por ello que decidi escojer una carrera acorde con lo que me habia graduado en el colegio </p>
+<p class="eys-parrafo"> El primer lenguaje con el cual tuve mi primer contacto con la programacion fue el  <b><a href="https://es.wikipedia.org/wiki/Genie_(lenguaje_de_programación)"> basic</a></b>  aun que en un inicio fue muy dificil los profesores hicieorn que el entender lo que era la programacion fuese mas sencillo.</p>
 
    <div id="blog-seccion2"  style="height: 50px;"></div>
 
@@ -34,23 +34,23 @@ include("marco-right.php");
     
 
 
-                   <p class="eys-parrafo">Mis estudios secundarios los realice en el colegio Fisco-Misional Sagado Corazon donde me gradue en el año 1989, de la especialidad de Físico-Matemático, apenas me gradue viaje a Guayaquil para seguir mis estudios universitarios en la Escuela Superior Politécnica del Litoral donde seguí la carrera de Ingeniería en Computación, gracias a la demanda que exitía en este tiempo de este tipo de profesionales comencé a trabajar mientra estaba estudiando, prestando mis servicios profesionales en empresas como Poligráfica, El telegráfo, Intecorsorcio; mientras trabajaba en esta última logré obtener mi título de Ingeniero de Computación.</p>
-                   <p class="eys-parrafo">Ya con el título en la mano decidi regrasar a mi tierra Esmeraldas, con la finalidad de aplicar los conocimiento y habilidades obtenida por lo que forme la empresa EDUCAYSOFT, pero como los ingreso no fueron suficiente entre a trabajar al colegio Eloy Alfaro y posteiormente a la Universidad Técnica Luis Vargas Torres donde actualmente laboró.</p>
-<p class="eys-parrafo"> Por las exigencias de la universidad, ingresar a la maestria de docencia y desarrollo del curriculo, de la cual de grauduo obteniendo el título de Magister, pero despues realice otra maestría en la POLITECNICA, de Ciencias de la Computación.</p>
+                   <p class="eys-parrafo">Mis estudios secundarios los realice en el colegio 5 de Agosto donde me gradue en el año 2017, de la especialidad de Informatica, apenas me gradue ingrese a la universidad publica de Esmeraldas.</p>
+                   <p class="eys-parrafo">aun no tengo planes claros para futuro lo unico que tengo en mente es aprobar mi semestre es lo que siempre tengo en mente cada vez que comienza un nuevo ciclo</p>
+<p class="eys-parrafo"> Aun que la universidad es muy complicada sigo adelante pensando en que ya me faltan pocos ciclos para lograr conseguir mi meta .</p>
    <div id="blog-seccion3"  style="height: 50px;"></div>
 
     <h2 style="border-bottom: 1px solid green;"> Mi perfil informático</h2> 
     
 
 
-                   <p  class="eys-parrafo">Mi perfil tecnológico es la programación de computadoras, mi primer programa lo hice en el leguaje Basic, utilizando el computador de la espol, luego programe c++, donde realiza algunos programa que leina el entregas del puerto serial y enviaban señales a un motor. Profesionalmente comenza programando en Informix un lenguaje de programación para el sistema Unix, utilizando uan versio de foxpro para unix, luego cuando aparecio la interface gráfica programe en foxpro y seguidamente visual foro.</p> 
-                   <p  class="eys-parrafo">Hoy en día creso sisemas web combinando HTML, CSS, JavaScrip y PHP; tambien aplicaciones para dispositivo androi para lo cual utilizo el framework android Studio, el cual utiliza el lenguaje de JAVA.</p>
+                   <p  class="eys-parrafo">Mi perfil tecnológico es la programación de computadoras, mi primer programa lo hice en el leguaje c#, utilizando el computador del colegio, luego programe c++, donde realiza algunos programas tipicos que aria un estudiante como una calculadora una agenda o un reloj.</p> 
+                   <p  class="eys-parrafo">Hoy en día creo sisemas web simples combinando HTML, CSS, JavaScrip y PHP.</p>
    <div id="blog-seccion4"  style="height: 50px;"></div>
 
     <h2 style="border-bottom: 1px solid green;"> Mis Hooby</h2> 
     
 
-                   <p   class="eys-parrafo">Soy amante a la lectura, así que en los ratos libres me encanta actualizar mis conocimientos leyendo manuales de los leguajes de progración que namejo, y tambien muchos noticias de las ultimas tendencias en tecnología.</p>
+                   <p   class="eys-parrafo">Me gusta mucho escuchar audio libros mientras realizo otras actividades secundarias como realizar mis tareas practicar programacion y etc.</p>
        
 
                    
